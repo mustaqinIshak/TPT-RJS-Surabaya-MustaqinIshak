a977ef9a14b0a9e7b7578ef8db3f9b18743d3d21
mustaqinIshak <mustaqinishak@gmail.com>
2022-11-30 13:09:15 +0800

    done

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [dataUser, setDataUser] = useState()
+  const [tertua, setTertua] = useState([])
+
+  const tertuaHandle = (payload) => {
+    let result = []
+    for(let i = 0; i < payload.length; i++) {
+      if(payload[i].height < 170 && payload[i].age >= 30) {
+        console.log("ada")
+        result.push(payload[i])
+      }
+    }
+    setTertua(...tertua, result)
+    console.log(result, "ini tertua")
+  }
+
+  useEffect(() => {
+    axios({
+      method:"get",
+      url:'https://dummyjson.com/users',
+    })
+    .then(data => {
+      tertuaHandle(data.data.users)
+      // console.log(data.data.users)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= "dataTertua">
+          {
+            tertua.map((item, key) => 
+              <div key={key} className="itemsTertua">
+                <div>
+                  <img src={item.image} />
+                </div>
+                <div>
+                  <span>Name:</span>
+                  {item.firstName + ' ' + item.lastName} 
+                </div>
+                <div>
+                  <span>Age:</span>
+                  {item.age}
+                </div>
+                <div>
+                  <span>Height:</span>
+                  {item.height}
+                </div>
+              </div>
+            )
+          }
+      </div>
     </div>
   );
 }
